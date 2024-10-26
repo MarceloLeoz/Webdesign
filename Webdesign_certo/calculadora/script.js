58274a62931f0055f49cf2766b5a7dcf8a8136ee
MarceloLeoz <marcelinho.leoz@icloud.com>
2024-10-26 14:43:30 -0300

    calculadora

---
+++ Webdesign_certo/calculadora/script.js
@@ -0,0 +1,8 @@
+const displayText = document.querySelector('.visor p');
+const buttons = Array.from(document.querySelectorAll('.button button'));
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayText.innerText += button.innerText;
+    });
+});
